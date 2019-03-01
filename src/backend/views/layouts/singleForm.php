c148a8466733c90d24cb4730d440e26d7990d7fa
Vitaliy <theyamshikov@yandex.ru>
2019-03-02 00:39:59 +0600

    fix

--- src/backend/views/layouts/singleForm.php
+++ src/backend/views/layouts/singleForm.php
@@ -3,9 +3,9 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use backend\assets\AppAsset;
 use yii2rails\extension\web\helpers\Page;
 use yii2lab\navigation\domain\widgets\Alert;
+use yii2lab\applicationTemplate\backend\assets\AppAsset;
 
 AppAsset::register($this);
 ?>
