313631a15408e24bf3847319315f7457125a90ad
Vitaliy <theyamshikov@yandex.ru>
2019-05-11 23:39:20 +0600

    test: upgrade data

--- src/common/data/user.php
+++ src/common/data/user.php
@@ -21,13 +21,13 @@ return [
 	],
 	[
 		'id' => 4,
-		'login' => 'reporter1',
+		'login' => 'developer1',
 		'status' => 1,
 		'created_at' => '2018-03-28 21:00:13',
 	],
 	[
 		'id' => 5,
-		'login' => 'reporter2',
+		'login' => 'developer2',
 		'status' => 1,
 		'created_at' => '2018-03-28 21:00:13',
 	],
