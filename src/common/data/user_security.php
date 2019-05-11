d71df26734d61a5dceecc7c446fe62ae007b9235
Vitaliy <theyamshikov@yandex.ru>
2019-05-11 18:47:28 +0600

    test: update user data

--- src/common/data/user_security.php
+++ src/common/data/user_security.php
@@ -4,48 +4,28 @@ use yii2module\account\domain\v3\helpers\UserFixture;
 
 return [
 	[
-		'id' => 381949,
+		'id' => 1,
 		'auth_key' => '4f6bbd8ea39e34f2f2d432a961be2a6a',
 		'password_hash' => UserFixture::PASSWORD_HASH,
 	],
 	[
-		'id' => 381069,
+		'id' => 2,
 		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg2',
 		'password_hash' => UserFixture::PASSWORD_HASH,
 	],
 	[
-		'id' => 381070,
+		'id' => 3,
 		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg3',
 		'password_hash' => UserFixture::PASSWORD_HASH,
 	],
 	[
-		'id' => 375664,
+		'id' => 4,
 		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg4',
 		'password_hash' => UserFixture::PASSWORD_HASH,
 	],
 	[
-		'id' => 381072,
+		'id' => 5,
 		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg5',
-		'password_hash' => UserFixture::PASSWORD_HASH,
-	],
-	[
-		'id' => 381073,
-		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg6',
-		'password_hash' => UserFixture::PASSWORD_HASH,
-	],
-	[
-		'id' => 381074,
-		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg7',
-		'password_hash' => UserFixture::PASSWORD_HASH,
-	],
-	[
-		'id' => 381075,
-		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg8',
-		'password_hash' => UserFixture::PASSWORD_HASH,
-	],
-	[
-		'id' => 381076,
-		'auth_key' => 'CUc6h7DyU07Lbru0QWu4-jb0Tiz15qWQZZ8IIix0sq5XO6-nWbtmImx93aTq4Cg9',
 		'password_hash' => UserFixture::PASSWORD_HASH,
 	],
 ];
